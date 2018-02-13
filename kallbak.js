9bc9e16e6782db948d663efa515084c454c7a490
kramduckner <buckner.a.mark@gmail.com>
2018-02-13 09:43:04 -0700

    Create kallbak.js

---
+++ kallbak.js
@@ -0,0 +1,8 @@
+function callFriend(callback){
+  callback();
+}
+
+//create a new function below that will be passed into callFriend(). This function that you create will 
+//console log "i'm calling my friend"
+
+//verify it works by 1. having no errors logged and 2. having "i'm calling my friend" logged
