1e02346e4b46ab4b3ae1639e0405bdbb2ea706a6
kramduckner <buckner.a.mark@gmail.com>
2018-02-13 09:49:54 -0700

    Create mark.js

---
+++ mark.js
@@ -0,0 +1,8 @@
+function yellAtMark(callback){
+  var string = "";
+  callback(string);
+}
+
+
+//call the function yellAtMark, passing in a function that console logs the value passed into your function you create. 
+//modify line 2 to be the thing you want to yell at mark
