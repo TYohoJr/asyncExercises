ecc96f63c01ec744a2c769238d7f389f89fc1a8a
kramduckner <buckner.a.mark@gmail.com>
2018-02-13 09:47:19 -0700

    Create katz.js

---
+++ katz.js
@@ -0,0 +1,13 @@
+function punchBag(callback){
+  let target = {
+    firstTarget: "center",
+    secondTarget: "outerRing"
+  };
+  callback(target);
+}
+
+//create a function, passing it into punchBag() below, that iterates over data and logs each value in the key-value pair
+
+punchBag();
+
+
