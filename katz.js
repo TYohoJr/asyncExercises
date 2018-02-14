d0d704cfa31a32c6227ba3490c547fa07cd17f17
Thomas Yoho <TYohoJr@Gmail.com>
2018-02-14 16:06:44 -0700

    finished katz.js

--- katz.js
+++ katz.js
@@ -6,8 +6,14 @@ function punchBag(callback){
   callback(target);
 }
 
+function myFunction(thing){
+  for(var key in thing){
+    console.log(thing[key])
+  }
+}
+
 //create a function, passing it into punchBag() below, that iterates over data and logs each value in the key-value pair
 
-punchBag();
+punchBag(myFunction);
 
 
